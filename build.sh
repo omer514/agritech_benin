2b24396064e7870e36f8b7efa0ad6cf433886983
omer514 <aidegoomer5@gmail.com>
2026-01-21 17:45:28 +0100

    des modifications

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ python manage.py migrate
 
 
 
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'aidegoomer5@gmail.com', 'admin1234') if not User.objects.filter(username='admin').exists() else None"
