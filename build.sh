4f49d734c2c8cea2b629354e616864471644d0af
omer514 <aidegoomer5@gmail.com>
2026-01-21 14:58:01 +0100

    Création automatique du superuser

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'aidegoomer5@gmail.com', 'admin1234') if not User.objects.filter(username='admin').exists() else None"
